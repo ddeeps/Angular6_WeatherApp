e2033c5e4a3d1b0905afe26589ea6677406d66be
drani <deepika.rani@yestelecom.nl>
2018-09-18 16:02:01 +0530

    in progress

--- WeatherApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ WeatherApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,12 +9,18 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdcYEKGIgkO7wnX_k1tbsAaYEiA6BvcD3zPAoBo8csaW8EAopR'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdcYEKGIgkO7wnX_k1tbsAaYEiA6BvcD3zPAoBo8csaW8EAopR')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdcYEKGIgkO7wnX_k1tbsAaYEiA6BvcD3zPAoBo8csaW8EAopR')
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
