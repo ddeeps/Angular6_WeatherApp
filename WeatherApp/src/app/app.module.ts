16f993bf8fdc245f06c94e656ec31ca50ccdf151
drani <deepika.rani@yestelecom.nl>
2018-12-07 19:52:53 +0530

    in progress

--- WeatherApp/src/app/app.module.ts
+++ WeatherApp/src/app/app.module.ts
@@ -1,35 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
-import { HomeComponent } from './home/home.component';
 import { SharedModules } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    AppRoutingModule,    
+    HttpClientModule,
+    AppRoutingModule,
     SharedModules,
     ShoppingListModule,
-    AuthModule
+    AuthModule,
+    CoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- WeatherApp/src/app/shared/data-storage.service.ts
+++ WeatherApp/src/app/shared/data-storage.service.ts
@@ -1,27 +1,26 @@
 import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 import 'rxjs/Rx';
 import { AuthService } from "../auth/auth.service";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class DataStorageService {
 
-    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) { }
+    constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
     storeRecipes() {
         const token = this.authService.getToken();
-        return this.http.put('https://ng-recipe-book-c5b22.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+        return this.httpClient.put('https://ng-recipe-book-c5b22.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
         const token = this.authService.getToken();
 
-        this.http.get('https://ng-recipe-book-c5b22.firebaseio.com/recipes.json?auth=' + token)
+        this.httpClient.get<Recipe[]>('https://ng-recipe-book-c5b22.firebaseio.com/recipes.json?auth=' + token)
             .map(
-                (response: Response) => {
-                    const recipes: Recipe[] = response.json();
+                (recipes) => {
                     for (let recipe of recipes) {
                         if (!recipe['ingredients']) {
                             recipe['ingredients'] = [];
